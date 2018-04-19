afc810605c715523f9f3672e25b5a258d48f54d9
YourtionGuo <yourtion@gmail.com>
2018-04-19 14:27:06 +0800

    添加 ISignOption、完善注释

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 /**
  * @file OSS Sign
- * @link https://help.aliyun.com/document_detail/31988.html
  * @author Yourtion Guo <yourtion@gmail.com>
  */
 
@@ -13,27 +12,54 @@ export interface IOption {
   endpoint?: string;
   expire?: number;
   defaultDir?: string;
+  maxSize?: number;
 }
 
+export interface ISignOption {
+  dir?: string;
+  expire?: number;
+  maxSize?: number;
+}
+
+export interface ISignResult {
+  policy: string;
+  signature: string;
+  expire: number;
+  accessid: string;
+  host?: string;
+  dir?: string;
+  key?: string;
+}
+
+interface IPolicys {
+  expiration: string;
+  conditions: Array<[string, string | number, string | number]>;
+}
+
+/**
+ * OSS 签名类
+ */
 export default class OSSSign {
   private accessKeyId: string;
   private accessKeySecret: string;
   private expire: number;
   private endpoint?: string;
   private defaultDir?: string;
+  private maxSize?: number;
 
   /**
    * 构造函数
-   * @param {IOption} options - 配置项
-   * @param {string} options.accessKeyId - 秘钥 AccessKeyId
-   * @param {string} options.accessKeySecret - 秘钥A ccessKeySecret
+   * @param {IOption} options 配置项
+   * @param {string} options.accessKeyId 秘钥ID AccessKeyId
+   * @param {string} options.accessKeySecret 秘钥Key AccessKeySecret
+   * @param {string} options.endpoint OSS地址
+   * @param {number} options.expire 默认过期时间（默认30s）
+   * @param {string} options.defaultDir 默认上传路径前缀
+   * @param {number} options.maxSize 默认最大文件大小（字节）
    */
   constructor(options: IOption) {
     assert(typeof options.accessKeyId === "string", "请配置 AccessKeyId");
-    assert(
-      typeof options.accessKeySecret === "string",
-      "请配置 AccessKeySecret"
-    );
+    assert(typeof options.accessKeySecret === "string", "请配置 AccessKeySecret");
     this.accessKeyId = options.accessKeyId;
     this.accessKeySecret = options.accessKeySecret;
     this.endpoint = options.endpoint;
@@ -45,23 +71,34 @@ export default class OSSSign {
     return crypto
       .createHmac("sha1", this.accessKeySecret)
       .update(data)
-      .digest()
-      .toString("base64");
+      .digest("base64");
   }
 
-  public sign(filename?: string) {
-    const expireAt = new Date().getTime() + this.expire;
+  /**
+   * 获取 OSS 签名
+   * @link https://help.aliyun.com/document_detail/31988.html
+   * @param {string} filename 文件名
+   * @param {ISignOption} options 签名参数
+   * @param {string} options.dir 目录前缀
+   * @param {number} options.maxSize 文件最大尺寸（字节）
+   * @param {number} options.expire 过期时间（秒）
+   */
+  public sign(filename?: string, options: ISignOption = {}): ISignResult {
+    const expireAt = new Date().getTime() + (options.expire || this.expire);
     const expiration = new Date(expireAt).toISOString();
-    const fielkey = filename
-      ? this.defaultDir + "/" + filename
-      : this.defaultDir;
-    const policy = {
-      expiration,
-      conditions: [
-        ["content-length-range", 0, 1048576000],
-        ["starts-with", "$key", fielkey]
-      ]
-    };
+    const policy: IPolicys = { expiration, conditions: [] };
+    if (options.maxSize || this.maxSize) {
+      policy.conditions.push(["content-length-range", 0, options.maxSize || this.maxSize!]);
+    }
+    const prefix = options.dir || this.defaultDir;
+    if (filename) {
+      // 存在文件名使用严格相等判断
+      const fielkey = prefix ? prefix + filename : filename;
+      policy.conditions.push(["eq", "$key", fielkey]);
+    } else if (prefix) {
+      // 存在目录使用key前缀限制
+      policy.conditions.push(["starts-with", "$key", prefix]);
+    }
     const policyStr = JSON.stringify(policy);
     const base64Policy = Buffer.from(policyStr).toString("base64");
     const signature = this.getHash(base64Policy);
@@ -72,7 +109,8 @@ export default class OSSSign {
       expire,
       accessid: this.accessKeyId,
       host: this.endpoint,
-      dir: this.defaultDir
+      dir: prefix,
+      key: (prefix || "") + (filename || "")
     };
   }
 }
